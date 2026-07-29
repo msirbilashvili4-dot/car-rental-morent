15c6dd36b48eed5ae8138a01efdd0ba9cc7467ea
Giga <giorgi.khundadze15b@geolab.edu.ge>
2026-07-29 21:43:25 +0400

    feat: improve homepage responsiveness and initialize page-specific functionality
    - Improved homepage responsiveness across multiple sections.
    - Refined the Pick-up & Drop-off section layout for smaller screens.
    - Adjusted the header layout to improve responsiveness.
    - Updated the cars wrapper layout for better mobile presentation.
    - Added responsive breakpoints for 500px, 480px, and 380px screen widths.
    
    JavaScript:
    - Added page detection based on the current pathname.
    - Automatically set today's date for date inputs on the homepage.
    - Automatically set the current time for time inputs on the homepage.
    - Added filter page initialization.

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-const carsArr=document.querySelectorAll(".wrapper article.card")
-const counterElement=document.querySelector(".counter-wrapper .counter-value")
+let currentPage = window.location.pathname
 
-counterElement.textContent=carsArr.length
+
+currentPage = currentPage.split('/')[1].split('.html')[0]
+
+
+
+
+
+if (currentPage === 'index') {
+
+    console.log('main page');
+
+    const pickupDropoffFilter = document.querySelector('section.pickup-dropoff-filter')
+
+    const inputs = pickupDropoffFilter.querySelectorAll('.filters-wrapper .filter input')
+
+    inputs.forEach((input) => {
+        if (input.type === 'date') {
+            input.value = new Date().toISOString().split('T')[0]
+        } else {
+            input.value = new Date().toTimeString().slice(0, 5);
+        }
+    })
+
+} else if (currentPage === 'filter') {
+    console.log('filters page');
+    const carsArr = document.querySelectorAll(".wrapper article.card")
+    const counterElement = document.querySelector(".counter-wrapper .counter-value")
+
+    counterElement.textContent = carsArr.length
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
