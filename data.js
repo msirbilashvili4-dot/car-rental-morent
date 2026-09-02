c9b6e84b47eddd9f9282ee641cf1113ba3a2bcd9
msirbilashvili4-dot <m.sirbilashvili4@gmail.com>
2026-09-02 21:41:01 +0400

    `feat: dynamically render car cards from car data`

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ export const cars = [
         type: "Sport",
         image: "assets/images/koenigsegg.png",
         price: 99.00, 
-        spesifications: {
+        specifications: {
             fuelCapacity: 90,
             transmission: "manual",
             seats: 2
@@ -15,7 +15,7 @@ export const cars = [
         type: "Sport",
         image: "assets/images/koenigsegg.png",
         price: 99.00, 
-        spesifications: {
+        specifications: {
             fuelCapacity: 90,
             transmission: "manual",
             seats: 2
@@ -26,7 +26,7 @@ export const cars = [
         type: "Sport",
         image: "assets/images/koenigsegg.png",
         price: 99.00, 
-        spesifications: {
+        specifications: {
             fuelCapacity: 90,
             transmission: "manual",
             seats: 2
@@ -37,7 +37,7 @@ export const cars = [
         type: "Sport",
         image: "assets/images/koenigsegg.png",
         price: 99.00, 
-        spesifications: {
+        specifications: {
             fuelCapacity: 90,
             transmission: "manual",
             seats: 2
@@ -48,7 +48,7 @@ export const cars = [
         type: "Sport",
         image: "assets/images/koenigsegg.png",
         price: 99.00, 
-        spesifications: {
+        specifications: {
             fuelCapacity: 90,
             transmission: "manual",
             seats: 2
@@ -59,7 +59,7 @@ export const cars = [
         type: "Sport",
         image: "assets/images/koenigsegg.png",
         price: 99.00, 
-        spesifications: {
+        specifications: {
             fuelCapacity: 90,
             transmission: "manual",
             seats: 2
@@ -71,38 +71,38 @@ export const cars = [
 
 /*
 <article class="card">
-                        <div class="header">
-                            <div>
-                                <h3>Koenigsegg</h3>
-                                <p>Sport</p>
-                            </div>
-                            <img src="assets/images/icons/heart.png" alt="heart icon">
-                        </div>
-                        <div class="image">
-                            <img src="assets/images/koenigsegg.png" alt="koenigsegg image">
-                        </div>
-                        <div class="spesifications">
-                            <div>
-                                <img src="assets/images/icons/gas-station.png" alt="gas-station icon">
-                                <span>90L</span>
-                            </div>
-                            <div>
-                                <img src="assets/images/icons/transmission.png" alt="transmission icon">
-                                <span>Manual</span>
-                            </div>
-                            <div>
-                                <img src="assets/images/icons/capacity.png" alt="capacity icon">
-                                <span>2 People</span>
-                            </div>
+    <div class="header">
+        <div>
+            <h3>Koenigsegg</h3>
+            <p>Sport</p>
+        </div>
+        <img src="assets/images/icons/heart.png" alt="heart icon">
+    </div>
+    <div class="image">
+        <img src="assets/images/koenigsegg.png" alt="koenigsegg image">
+    </div>
+    <div class="spesifications">
+        <div>
+            <img src="assets/images/icons/gas-station.png" alt="gas-station icon">
+            <span>90L</span>
+        </div>
+        <div>
+            <img src="assets/images/icons/transmission.png" alt="transmission icon">
+            <span>Manual</span>
+        </div>
+        <div>
+            <img src="assets/images/icons/capacity.png" alt="capacity icon">
+            <span>2 People</span>
+        </div>
 
-                        </div>
-                        <div class="footer">
-                            <div class="price-wrapper">
-                                <span>$99.00/</span>
-                                <span>day</span>
-                            </div>
-                            <div class="rent-now">
-                                <a href="#">Rent Now</a>
-                            </div>
-                        </div>
-                    </article>*/
+    </div>
+    <div class="footer">
+        <div class="price-wrapper">
+            <span>$99.00/</span>
+            <span>day</span>
+        </div>
+        <div class="rent-now">
+            <a href="#">Rent Now</a>
+        </div>
+    </div>
+</article>*/
